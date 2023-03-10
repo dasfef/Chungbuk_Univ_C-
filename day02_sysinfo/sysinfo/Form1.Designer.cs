43f58baf0573fafd69abd048aee679d8f8d7a5c7
dasfef <96065385+dasfef@users.noreply.github.com>
2023-03-10 17:13:12 +0900

    Add files via upload

---
+++ day02_sysinfo/sysinfo/Form1.Designer.cs
@@ -0,0 +1,83 @@
+﻿namespace sysinfo
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.DarkSalmon;
+            this.button1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.button1.Font = new System.Drawing.Font("Tahoma", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.Color.DarkRed;
+            this.button1.Location = new System.Drawing.Point(0, 0);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(430, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Get System";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.Color.Black;
+            this.textBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.textBox1.Font = new System.Drawing.Font("Tahoma", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.ForeColor = System.Drawing.Color.Lime;
+            this.textBox1.Location = new System.Drawing.Point(0, 23);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.textBox1.Size = new System.Drawing.Size(430, 427);
+            this.textBox1.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(430, 450);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Sysinfo";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
---
+++ day02_sysinfo/sysinfo/Form1.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net;
+using System.IO;
+using System.Security.Cryptography;
+using System.Diagnostics;
+
+namespace sysinfo
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void System_ID()
+        {
+            textBox1.Text = "";
+            textBox1.Text += "User : " + Environment.UserName + "\r\n";
+            string host = Dns.GetHostName();
+            textBox1.Text += "Host Name : " + host + "\r\n";
+            textBox1.Text += "Local IP Adress : " + Dns.GetHostByName(host).AddressList[0].ToString() + "\r\n";
+            textBox1.Text += "\r\n";
+        }
+
+        private void OS_Info()
+        {
+            textBox1.Text += "OS Platform : " + Environment.OSVersion.Platform + "\r\n";
+            textBox1.Text += "OS Ver : " + Environment.OSVersion + "\r\n";
+            if (Environment.Is64BitOperatingSystem) textBox1.Text += "64Bit Operating System" + "\r\n";
+            textBox1.Text += "Process Count : " + Environment.ProcessorCount + "\r\n";
+
+            TimeSpan TickTime = TimeSpan.FromMilliseconds(Environment.TickCount);
+            /*using (PerformanceCounter pc = new PerformanceCounter("System", "System Up Time"))
+            {
+                pc.NextValue();
+                textBox1.Text += "System Elapsed Time : " + TimeSpan.FromMilliseconds(pc.NextValue()).ToString();
+            }*/
+            textBox1.Text += "System Elapsed Time : " + TickTime.ToString(@"dd\:hh\:mm\:ss\:fff") + "\r\n";
+            textBox1.Text += "\r\n";
+        }
+
+        private void Drive_Info()
+        {
+            DriveInfo[] allDrives = DriveInfo.GetDrives();
+            foreach (DriveInfo drv in allDrives)
+            {
+                textBox1.Text += "Drive : " + drv.Name + "\r\n";
+                textBox1.Text += "  File type : " + drv.DriveType + "\r\n";
+                if (drv.IsReady == true)
+                {
+                    textBox1.Text += "  Volume label : " + drv.VolumeLabel + "\r\n";
+                    textBox1.Text += "  File system : " + drv.DriveFormat + "\r\n";
+                    textBox1.Text += string.Format("  Available space to current user : {0:###,##0} bytes \r\n", drv.AvailableFreeSpace);
+                    textBox1.Text += string.Format("  Total available space : {0:###,##0} bytes \r\n", drv.TotalFreeSpace);
+                    textBox1.Text += string.Format("  Total size of drive : {0:###,##0} bytes \r\n", drv.TotalSize);
+                }
+                textBox1.Text += "\r\n";
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            System_ID();
+            OS_Info();
+            Drive_Info();
+        }
+    }
+}
