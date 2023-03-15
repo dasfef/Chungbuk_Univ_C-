8dc8f0f66cdd49970c79545475e52f72c1706640
dasfef <96065385+dasfef@users.noreply.github.com>
2023-03-15 17:20:51 +0900

    Add files via upload

---
+++ day03_updTalk/udpTalk/Form1.Designer.cs
@@ -0,0 +1,207 @@
+﻿namespace udpTalk
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtPort = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtMyname = new System.Windows.Forms.TextBox();
+            this.btnConnect = new System.Windows.Forms.Button();
+            this.MsgWindow = new System.Windows.Forms.RichTextBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtSendToIP = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtSendMsg = new System.Windows.Forms.TextBox();
+            this.btnSend = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.btnConnect);
+            this.groupBox1.Controls.Add(this.txtMyname);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.txtPort);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.groupBox1.Location = new System.Drawing.Point(0, 0);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(743, 54);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 23);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(58, 12);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "LocalPort";
+            // 
+            // txtPort
+            // 
+            this.txtPort.Location = new System.Drawing.Point(77, 18);
+            this.txtPort.Name = "txtPort";
+            this.txtPort.Size = new System.Drawing.Size(100, 21);
+            this.txtPort.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(284, 22);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(65, 12);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "UserName";
+            // 
+            // txtMyname
+            // 
+            this.txtMyname.Location = new System.Drawing.Point(369, 18);
+            this.txtMyname.Name = "txtMyname";
+            this.txtMyname.Size = new System.Drawing.Size(100, 21);
+            this.txtMyname.TabIndex = 3;
+            // 
+            // btnConnect
+            // 
+            this.btnConnect.Location = new System.Drawing.Point(503, 18);
+            this.btnConnect.Name = "btnConnect";
+            this.btnConnect.Size = new System.Drawing.Size(75, 23);
+            this.btnConnect.TabIndex = 4;
+            this.btnConnect.Text = "Connect";
+            this.btnConnect.UseVisualStyleBackColor = true;
+            this.btnConnect.Click += new System.EventHandler(this.btnConnect_Click);
+            // 
+            // MsgWindow
+            // 
+            this.MsgWindow.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.MsgWindow.Location = new System.Drawing.Point(0, 54);
+            this.MsgWindow.Name = "MsgWindow";
+            this.MsgWindow.ReadOnly = true;
+            this.MsgWindow.ScrollBars = System.Windows.Forms.RichTextBoxScrollBars.Vertical;
+            this.MsgWindow.Size = new System.Drawing.Size(743, 396);
+            this.MsgWindow.TabIndex = 1;
+            this.MsgWindow.Text = "";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.btnSend);
+            this.groupBox2.Controls.Add(this.txtSendMsg);
+            this.groupBox2.Controls.Add(this.label4);
+            this.groupBox2.Controls.Add(this.txtSendToIP);
+            this.groupBox2.Controls.Add(this.label3);
+            this.groupBox2.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.groupBox2.Location = new System.Drawing.Point(0, 350);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(743, 100);
+            this.groupBox2.TabIndex = 2;
+            this.groupBox2.TabStop = false;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(17, 30);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(53, 12);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "Send To";
+            // 
+            // txtSendToIP
+            // 
+            this.txtSendToIP.Location = new System.Drawing.Point(102, 21);
+            this.txtSendToIP.Name = "txtSendToIP";
+            this.txtSendToIP.Size = new System.Drawing.Size(100, 21);
+            this.txtSendToIP.TabIndex = 1;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(17, 58);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(58, 12);
+            this.label4.TabIndex = 2;
+            this.label4.Text = "Message";
+            // 
+            // txtSendMsg
+            // 
+            this.txtSendMsg.Location = new System.Drawing.Point(102, 55);
+            this.txtSendMsg.Name = "txtSendMsg";
+            this.txtSendMsg.Size = new System.Drawing.Size(476, 21);
+            this.txtSendMsg.TabIndex = 3;
+            // 
+            // btnSend
+            // 
+            this.btnSend.Location = new System.Drawing.Point(598, 53);
+            this.btnSend.Name = "btnSend";
+            this.btnSend.Size = new System.Drawing.Size(75, 23);
+            this.btnSend.TabIndex = 4;
+            this.btnSend.Text = "Send";
+            this.btnSend.UseVisualStyleBackColor = true;
+            this.btnSend.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(743, 450);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.MsgWindow);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button btnConnect;
+        private System.Windows.Forms.TextBox txtMyname;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtPort;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.RichTextBox MsgWindow;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Button btnSend;
+        private System.Windows.Forms.TextBox txtSendMsg;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtSendToIP;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ day03_updTalk/udpTalk/Form1.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+
+namespace udpTalk
+{
+    public partial class Form1 : Form
+    {
+        private Socket UdpServer;
+        private IPEndPoint Listen_IPEndPoint;
+
+        private EndPoint Remote;
+        private int LocalPort = 9999;
+        private string BroadCast;
+        private string UserName;
+
+        delegate void StringArgReturningVoidDelegate(string text);
+        public Form1()
+        {
+            InitializeComponent();
+            txtPort.Text = LocalPort.ToString();
+        }
+
+        private void SetText(string text)
+        {
+            if (this.MsgWindow.InvokeRequired)
+            {
+                StringArgReturningVoidDelegate d = new StringArgReturningVoidDelegate(SetText);
+                this.Invoke(d, new object[] { text });
+            }
+            else
+            {
+                this.MsgWindow.AppendText(text);
+                MsgWindow.ScrollToCaret();
+            }
+        }
+
+        private void threadProc()
+        {
+            try
+            {
+                int receive;
+                string message;
+                byte[] rxBytes = new byte[2048];
+
+                while (true)
+                {
+                    receive = UdpServer.ReceiveFrom(rxBytes, ref Remote);
+                    message = Encoding.UTF8.GetString(rxBytes, 0, receive);
+                    SetText(message + "\r\n");
+                    Thread.Sleep(500);
+                }
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+                DisConnect();
+            }
+        }
+
+        private void InterConnect()
+        {
+            LocalPort = Convert.ToInt32(txtPort.Text, 10);
+            UserName = txtMyname.Text;
+            UdpServer = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+
+            Listen_IPEndPoint = new IPEndPoint(IPAddress.Any, LocalPort);
+            Remote = (EndPoint)Listen_IPEndPoint;
+            UdpServer.Bind(Listen_IPEndPoint);
+
+            Thread thread = new Thread(new ThreadStart(threadProc));
+
+            thread.IsBackground = true;
+            thread.Start();
+
+            SetText("Listen -\r\n");
+            btnConnect.Text = "Close";
+        }
+
+        private void DisConnect()
+        {
+            Thread.EndThreadAffinity();
+            UdpServer.Dispose();
+
+            SetText("DisConnect -\r\n");
+            btnConnect.Text = "Connect";
+        }
+
+        private void SendProc(string broadcast, string user, String msg)
+        {
+            try
+            {
+                IPEndPoint Sender = new IPEndPoint(IPAddress.Parse(broadcast), LocalPort);
+                byte[] SendData = Encoding.UTF8.GetBytes(user + " > " + msg + "\n");
+                UdpServer.SendTo(SendData, SendData.Length, SocketFlags.None, Sender);
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+        }
+
+        private void btnConnect_Click(object sender, EventArgs e)
+        {
+            if (btnConnect.Text == "Connect") InterConnect();
+            else DisConnect();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            BroadCast = txtSendToIP.Text;
+            SendProc(BroadCast, UserName, txtSendMsg.Text);
+            txtSendMsg.Text = "";
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            DisConnect();
+        }
+    }
+}
