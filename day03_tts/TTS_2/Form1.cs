d957c45ed04b47eb02828978e566d6c5fe6bdaba
dasfef <96065385+dasfef@users.noreply.github.com>
2023-03-15 16:41:20 +0900

    Add files via upload

---
+++ day03_tts/TTS_2/Form1.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Speech.Synthesis;
+
+namespace TTS_2
+{
+    public partial class Form1 : Form
+    {
+        SpeechSynthesizer synth = new SpeechSynthesizer();
+        Int16 narratorIndex = 0;
+        List<string> narratorName = new List<string>();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public void narratorPicture(Int16 index)
+        {
+            switch(index)
+            {
+                case 0:
+                    pictureBox1.BackgroundImage = Properties.Resources.heami;
+                    break;
+                case 1:
+                    pictureBox1.BackgroundImage = Properties.Resources.Zira;
+                    break;
+                case 2:
+                    pictureBox1.BackgroundImage = Properties.Resources.david;
+                    break;
+                default:
+                    narratorIndex = 0;
+                    pictureBox1.BackgroundImage = Properties.Resources.heami;
+                    break;
+
+
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            foreach (InstalledVoice voice in synth.GetInstalledVoices())
+            {
+                VoiceInfo info = voice.VoiceInfo;
+                narratorName.Add(info.Name);
+            }
+            narratorPicture(narratorIndex);
+
+            synth.SetOutputToDefaultAudioDevice();
+            synth.SelectVoice(narratorName[narratorIndex]);
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            if (++narratorIndex > 2) narratorIndex = 0;
+
+            narratorPicture(narratorIndex);
+            synth.SelectVoice(narratorName[narratorIndex]);
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            DateTime now = DateTime.Now;
+            label1.Text = now.ToString("HH:mm:ss");
+
+            if(now.Second == 0)
+            {
+                string message = String.Format("현재 시각은 {0}시{1}분 입니다", now.Hour, now.Minute);
+                synth.Speak(message);
+            }
+            label1.Text = now.ToString("HH:mm:ss");
+        }
+    }
+}
---
+++ day03_tts/tts/Form1.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Speech.Synthesis;
+
+namespace tts
+{
+    
+    public partial class Form1 : Form
+    {
+        SpeechSynthesizer synth = new SpeechSynthesizer();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            cmbSpeaker.Items.Clear();
+            foreach(InstalledVoice voice in synth.GetInstalledVoices())
+            {
+                VoiceInfo info = voice.VoiceInfo;
+                cmbSpeaker.Items.Add(info.Name);
+            }
+
+            cmbSpeaker.SelectedIndex = 0;
+
+            synth.SetOutputToDefaultAudioDevice();
+            synth.SelectVoice(cmbSpeaker.Text);
+        }
+
+        private void cmbSpeaker_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (cmbSpeaker.Text.Length > 0)
+            {
+                synth.SelectVoice(cmbSpeaker.Text);
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text.Length > 0 )
+            {
+                synth.Speak(textBox1.Text);
+            }
+            else
+            {
+                synth.Speak("메시지를 넣어주세요");
+            }
+        }
+    }
+}
