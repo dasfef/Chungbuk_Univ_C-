f7ab39b4d3c5c127c2323929f4bb22d215d68ed0
dasfef <96065385+dasfef@users.noreply.github.com>
2023-03-15 16:37:48 +0900

    Add files via upload

---
+++ day03_fileExplorer/File_Explorer/Form1.Designer.cs
@@ -0,0 +1,113 @@
+﻿namespace File_Explorer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.splitContainer2 = new System.Windows.Forms.SplitContainer();
+            this.treeView1 = new System.Windows.Forms.TreeView();
+            this.listView1 = new System.Windows.Forms.ListView();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer2)).BeginInit();
+            this.splitContainer2.Panel1.SuspendLayout();
+            this.splitContainer2.Panel2.SuspendLayout();
+            this.splitContainer2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // textBox2
+            // 
+            this.textBox2.Dock = System.Windows.Forms.DockStyle.Top;
+            this.textBox2.Location = new System.Drawing.Point(0, 0);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(736, 21);
+            this.textBox2.TabIndex = 0;
+            // 
+            // splitContainer2
+            // 
+            this.splitContainer2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer2.Location = new System.Drawing.Point(0, 21);
+            this.splitContainer2.Name = "splitContainer2";
+            // 
+            // splitContainer2.Panel1
+            // 
+            this.splitContainer2.Panel1.Controls.Add(this.treeView1);
+            // 
+            // splitContainer2.Panel2
+            // 
+            this.splitContainer2.Panel2.Controls.Add(this.listView1);
+            this.splitContainer2.Size = new System.Drawing.Size(736, 422);
+            this.splitContainer2.SplitterDistance = 265;
+            this.splitContainer2.TabIndex = 1;
+            // 
+            // treeView1
+            // 
+            this.treeView1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.treeView1.Location = new System.Drawing.Point(0, 0);
+            this.treeView1.Name = "treeView1";
+            this.treeView1.Size = new System.Drawing.Size(265, 422);
+            this.treeView1.TabIndex = 0;
+            this.treeView1.BeforeCollapse += new System.Windows.Forms.TreeViewCancelEventHandler(this.treeView1_BeforeCollapse);
+            this.treeView1.BeforeExpand += new System.Windows.Forms.TreeViewCancelEventHandler(this.treeView1_BeforeExpand);
+            this.treeView1.NodeMouseClick += new System.Windows.Forms.TreeNodeMouseClickEventHandler(this.treeView1_NodeMouseClick);
+            // 
+            // listView1
+            // 
+            this.listView1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.listView1.HideSelection = false;
+            this.listView1.Location = new System.Drawing.Point(0, 0);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(467, 422);
+            this.listView1.TabIndex = 0;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            // 
+            // Form1
+            // 
+            this.ClientSize = new System.Drawing.Size(736, 443);
+            this.Controls.Add(this.splitContainer2);
+            this.Controls.Add(this.textBox2);
+            this.Name = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.splitContainer2.Panel1.ResumeLayout(false);
+            this.splitContainer2.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer2)).EndInit();
+            this.splitContainer2.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.SplitContainer splitContainer2;
+        private System.Windows.Forms.TreeView treeView1;
+        private System.Windows.Forms.ListView listView1;
+    }
+}
+
---
+++ day03_imageviewer/imageViewer/Form1.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace imageViewer
+{
+    public partial class Form1 : Form
+    {
+        string FullName;
+        string ShortName;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            pictureBox1.BorderStyle = BorderStyle.FixedSingle;
+            pictureBox1.BackColor = Color.White;
+            pictureBox1.SizeMode = PictureBoxSizeMode.AutoSize;
+
+            ShortName = "제목없음";
+            this.Text = ShortName + " - ImageViewer";
+        }
+
+        private void mnuEnd_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void mnuLoad_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog OpenFile = new OpenFileDialog();
+
+            OpenFile.Title = "Origin Image Open";
+            OpenFile.InitialDirectory = Environment.CurrentDirectory;
+            OpenFile.Filter = "Image Files|*.jpg;*.jpeg;*.gif;*.bmp;*.png|All Files(*.*)|*.*";
+
+            if (OpenFile.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                FullName = OpenFile.FileName;
+                FileInfo fi = new FileInfo(FullName);
+                ShortName = fi.Name;
+
+                pictureBox1.Image = Image.FromFile(FullName);
+                this.Text = ShortName + " - ImageViewer";
+            }
+        }
+
+        private void mnuSaveAs_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog SaveFile = new SaveFileDialog();
+
+            SaveFile.Title = "다른 이름으로 저장";
+            SaveFile.InitialDirectory = Environment.CurrentDirectory;
+            SaveFile.Filter = "Image Files|*.jpg;*.jpeg;*.gif;*.bmp;*.png|All Files(*.*)|*.*";
+            SaveFile.FileName = ShortName;
+
+            if (SaveFile.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                FullName = SaveFile.FileName;
+                FileInfo fi = new FileInfo(FullName);
+                ShortName = fi.Name;
+
+                this.Text = ShortName + " - ImageViewer";
+                pictureBox1.Image.Save(FullName);
+            }
+        }
+    }
+}
