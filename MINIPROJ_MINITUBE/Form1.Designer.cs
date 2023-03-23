54de1530f97d0f2c1a5a0af32ba3586797e42aae
dasfef <96065385+dasfef@users.noreply.github.com>
2023-03-23 18:17:38 +0900

    Add files via upload

---
+++ MINIPROJ_MINITUBE/Form1.Designer.cs
@@ -0,0 +1,218 @@
+﻿namespace MINITUBE
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.btnSearch = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.groupBox4 = new System.Windows.Forms.GroupBox();
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.toolStripTextBox1 = new System.Windows.Forms.ToolStripTextBox();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            this.groupBox4.SuspendLayout();
+            this.toolStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(7, 19);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(168, 21);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.KeyDown += new System.Windows.Forms.KeyEventHandler(this.textBox1_KeyDown);
+            // 
+            // btnSearch
+            // 
+            this.btnSearch.Location = new System.Drawing.Point(181, 19);
+            this.btnSearch.Name = "btnSearch";
+            this.btnSearch.Size = new System.Drawing.Size(75, 23);
+            this.btnSearch.TabIndex = 1;
+            this.btnSearch.Text = "YouTube Search";
+            this.btnSearch.UseVisualStyleBackColor = true;
+            this.btnSearch.Click += new System.EventHandler(this.btnSearch_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.listView1);
+            this.groupBox1.Controls.Add(this.groupBox2);
+            this.groupBox1.Dock = System.Windows.Forms.DockStyle.Left;
+            this.groupBox1.Location = new System.Drawing.Point(0, 0);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(305, 903);
+            this.groupBox1.TabIndex = 2;
+            this.groupBox1.TabStop = false;
+            // 
+            // listView1
+            // 
+            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1});
+            this.listView1.GridLines = true;
+            this.listView1.HideSelection = false;
+            this.listView1.Location = new System.Drawing.Point(3, 113);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(299, 900);
+            this.listView1.TabIndex = 0;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            this.listView1.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.listView1_MouseDoubleClick);
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "TITLE";
+            this.columnHeader1.Width = 295;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.button1);
+            this.groupBox2.Controls.Add(this.textBox1);
+            this.groupBox2.Controls.Add(this.btnSearch);
+            this.groupBox2.Controls.Add(this.textBox2);
+            this.groupBox2.Location = new System.Drawing.Point(21, 17);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(264, 81);
+            this.groupBox2.TabIndex = 7;
+            this.groupBox2.TabStop = false;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(181, 47);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 21);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "ENTER";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.button1.KeyDown += new System.Windows.Forms.KeyEventHandler(this.button1_KeyDown);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(7, 46);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(168, 21);
+            this.textBox2.TabIndex = 4;
+            this.textBox2.KeyDown += new System.Windows.Forms.KeyEventHandler(this.textBox2_KeyDown);
+            // 
+            // panel1
+            // 
+            this.panel1.AutoScroll = true;
+            this.panel1.AutoSize = true;
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel1.Location = new System.Drawing.Point(3, 17);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(611, 883);
+            this.panel1.TabIndex = 6;
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.panel1);
+            this.groupBox3.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.groupBox3.Location = new System.Drawing.Point(305, 0);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(617, 903);
+            this.groupBox3.TabIndex = 8;
+            this.groupBox3.TabStop = false;
+            // 
+            // groupBox4
+            // 
+            this.groupBox4.Controls.Add(this.toolStrip1);
+            this.groupBox4.Dock = System.Windows.Forms.DockStyle.Top;
+            this.groupBox4.Location = new System.Drawing.Point(305, 0);
+            this.groupBox4.Name = "groupBox4";
+            this.groupBox4.Size = new System.Drawing.Size(617, 46);
+            this.groupBox4.TabIndex = 9;
+            this.groupBox4.TabStop = false;
+            // 
+            // toolStrip1
+            // 
+            this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripTextBox1});
+            this.toolStrip1.Location = new System.Drawing.Point(3, 17);
+            this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.Size = new System.Drawing.Size(611, 25);
+            this.toolStrip1.TabIndex = 0;
+            this.toolStrip1.Text = "toolStrip1";
+            // 
+            // toolStripTextBox1
+            // 
+            this.toolStripTextBox1.Name = "toolStripTextBox1";
+            this.toolStripTextBox1.Size = new System.Drawing.Size(500, 25);
+            this.toolStripTextBox1.KeyDown += new System.Windows.Forms.KeyEventHandler(this.toolStripTextBox1_KeyDown_1);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(922, 903);
+            this.Controls.Add(this.groupBox4);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "MiniTube";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox3.PerformLayout();
+            this.groupBox4.ResumeLayout(false);
+            this.groupBox4.PerformLayout();
+            this.toolStrip1.ResumeLayout(false);
+            this.toolStrip1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button btnSearch;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.GroupBox groupBox4;
+        private System.Windows.Forms.ToolStrip toolStrip1;
+        private System.Windows.Forms.ToolStripTextBox toolStripTextBox1;
+    }
+}
+
---
+++ MINIPROJ_MINITUBE/Form1.cs
@@ -0,0 +1,164 @@
+﻿using Google.Apis.Services;
+using Google.Apis.YouTube.v3;
+using CefSharp;
+using CefSharp.WinForms;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Diagnostics;
+
+namespace MINITUBE
+{
+    public partial class Form1 : Form
+    {
+        
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            CefSettings settings = new CefSettings();
+            Cef.Initialize(settings);
+            string URL = "https://dasfef.github.io";
+            ChromiumWebBrowser chrome = new ChromiumWebBrowser(URL);
+            panel1.Controls.Add(chrome);
+            //chrome.AddressChanged += Chrome_AddressChanged;                                       // 현주소 필요할시 요청
+
+        }
+
+        // ===== 현재 주소 필요할 때 활성화 =====
+        private void Chrome_AddressChanged(object sender, AddressChangedEventArgs e)
+        {
+            this.Invoke(new MethodInvoker(() =>
+            {
+                //textBox2.Text = e.Address;
+                toolStripTextBox1.Text = e.Address;
+            }));
+        }
+
+        // ===== 유튜브 검색 후 결과 리스트뷰 위치 =====
+        private async void btnSearch_Click(object sender, EventArgs e)
+        {
+            listView1.Items.Clear();
+            var youtube = new YouTubeService(new BaseClientService.Initializer()
+            {
+                ApiKey = "AIzaSyDzBgk2gxfcAiOU2whxxOa62fPImBEDVVA",
+                ApplicationName = "MiniTube"
+            });
+
+            var request = youtube.Search.List("snippet");
+            request.Q = textBox1.Text;
+            request.MaxResults = 25;
+
+            var result = await request.ExecuteAsync();
+
+            foreach (var item in result.Items)
+            {
+                if(item.Id.Kind == "youtube#video")
+                {
+                    listView1.Items.Add(item.Id.VideoId.ToString(), item.Snippet.Title, 0);
+                }
+            }
+        }
+
+        // ===== 리스트뷰 목록 더블클릭시 -> 바로 해당 유튜브 링크로 이동 =====
+        private void listView1_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            if(listView1.SelectedItems.Count > 0)
+            {
+                panel1.Controls.Clear();
+                string videoId = listView1.SelectedItems[0].Name;
+                string youtubeURL = "http://youtube.com/watch?v=" + videoId;
+
+                ChromiumWebBrowser chrome = new ChromiumWebBrowser(youtubeURL);
+                panel1.Controls.Add(chrome);
+                //ChromiumWebBrowser youtubeUrl = new ChromiumWebBrowser(youtubeURL);
+                
+                
+            }
+        }
+
+        // ===== GO 버튼 클릭 시 해당 링크의 주소로 이동 =====
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string text2 = textBox2.Text;
+            panel1.Controls.Clear();
+            
+            ChromiumWebBrowser chrome = new ChromiumWebBrowser(text2);
+            panel1.Controls.Add(chrome);
+            //chrome.AddressChanged += Chrome_AddressChanged;                                       // 현주소 필요할시 요청
+        }
+
+
+        // ===== 주소창 엔터키 입력시 링크의 주소로 이동 =====
+        private void toolStripTextBox1_KeyDown_1(object sender, KeyEventArgs e)
+        {
+            string url = toolStripTextBox1.Text;
+            if (e.KeyCode == Keys.Enter)
+            {
+                panel1.Controls.Clear();
+
+                ChromiumWebBrowser chrome = new ChromiumWebBrowser(url);
+                panel1.Controls.Add(chrome);
+                chrome.AddressChanged += Chrome_AddressChanged;                                     // 현주소 필요할시 요청
+            }
+        }
+
+        // ===== 버튼1 에서 엔터 쳤을 때 =====
+        private void button1_KeyDown(object sender, KeyEventArgs e)
+        {
+            string text2 = textBox2.Text;
+            
+
+            if (e.KeyCode == Keys.Enter)
+            {
+                panel1.Controls.Clear();
+
+                ChromiumWebBrowser chrome = new ChromiumWebBrowser(text2);
+                panel1.Controls.Add(chrome);
+            }
+        }
+
+        // ===== text2 에서 엔터 쳤을 때 =====
+        private void textBox2_KeyDown(object sender, KeyEventArgs e)
+        {
+            string text2 = textBox2.Text;
+
+
+            if (e.KeyCode == Keys.Enter)
+            {
+                panel1.Controls.Clear();
+
+                ChromiumWebBrowser chrome = new ChromiumWebBrowser(text2);
+                panel1.Controls.Add(chrome);
+            }
+        }
+
+        // ===== 유튜브 검색창에서 엔터키 입력시 =====
+        private void textBox1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                if (listView1.SelectedItems.Count > 0)
+                {
+                    panel1.Controls.Clear();
+                    string videoId = listView1.SelectedItems[0].Name;
+                    string youtubeURL = "http://youtube.com/watch?v=" + videoId;
+
+                    ChromiumWebBrowser chrome = new ChromiumWebBrowser(youtubeURL);
+                    panel1.Controls.Add(chrome);
+                    //ChromiumWebBrowser youtubeUrl = new ChromiumWebBrowser(youtubeURL);
+
+                }
+            }
+        }
+    }
+}
